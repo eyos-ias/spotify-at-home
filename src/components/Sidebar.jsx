d4de52bccaef43c5f970e3c8f7c78bf555666c00
eyos <eyosiastamirat20@gmail.com>
2022-12-12 05:58:48 +0300

    ...

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -36,6 +36,7 @@ const Sidebar = () => {
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
+        <p className='text-white/30 align-text-bottom'>made with <span className='text-red-700'>♥</span> by eyos</p>
       </div>
 
       {/* Mobile sidebar */}
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,12 +1,12 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-
+//import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
 
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
         prepareHeaders: (headers)=>{
-            headers.set('X-RapidAPI-Key', '388e168078msh714413eee6263dfp18ec05jsn5203b411a0b7')
+            headers.set('X-RapidAPI-Key', `${import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY}`)
         
             return headers;
         }
